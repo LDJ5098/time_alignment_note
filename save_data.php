cfe27c3eb6d5486dbc8ccdc0fed5f338ec43c265
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2023-11-25 21:17:52 +0900

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -1,30 +1,26 @@
 <?php
-// 데이터베이스 연결 설정
-$servername = "localhost"; // 또는 '119.200.201.60'
+$servername = "localhost";
 $username = "timememo_user";
 $password = "5097";
 $dbname = "timememoDB";
 
-// POST 요청으로 전송된 데이터 가져오기
-$ID = $_POST['ID'];
-$mainlist = $_POST['mainlist'];
-
-// 데이터베이스 연결
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// 연결 확인
 if ($conn->connect_error) {
     die("데이터베이스 연결 실패: " . $conn->connect_error);
 }
 
-// 데이터베이스에 데이터 삽입
-$sql = "INSERT INTO user_timememo (ID, mainlist) VALUES ('$ID', '" . json_encode($mainlist) . "')";
+$ID = $conn->real_escape_string($_POST['ID']);
+$mainlist = $conn->real_escape_string($_POST['mainlist']);
+
+$sql = "INSERT INTO user_timememo (ID, mainlist) VALUES ('$ID', '$mainlist')";
+
 if ($conn->query($sql) === TRUE) {
     echo "데이터베이스에 데이터 삽입 성공";
 } else {
     echo "데이터베이스에 데이터 삽입 실패: " . $conn->error;
+    error_log("SQL 에러: " . $conn->error);
 }
 
-// 데이터베이스 연결 종료
 $conn->close();
 ?>
