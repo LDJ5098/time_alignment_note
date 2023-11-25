4cf2ea1c7cb1953d4fe755466948c1980338028c
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2023-11-25 20:32:23 +0900

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -1,28 +1,30 @@
 <?php
-$servername = "119.200.201.60";
+// 데이터베이스 연결 설정
+$servername = "localhost"; // 또는 '119.200.201.60'
 $username = "timememo_user";
 $password = "5097";
-$database = "timememo_userdatabase";
+$dbname = "timememoDB";
 
-$conn = new mysqli($servername, $username, $password, $database);
+// POST 요청으로 전송된 데이터 가져오기
+$ID = $_POST['ID'];
+$mainlist = $_POST['mainlist'];
 
+// 데이터베이스 연결
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 연결 확인
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("데이터베이스 연결 실패: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userID = $_POST["userID"];
-    $memo = $_POST["memo"];
-
-    // 데이터베이스에 데이터 저장
-    $sql = "INSERT INTO mainlist (userID, memo) VALUES ('$userID', '$memo')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Data saved successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+// 데이터베이스에 데이터 삽입
+$sql = "INSERT INTO user_timememo (ID, mainlist) VALUES ('$ID', '" . json_encode($mainlist) . "')";
+if ($conn->query($sql) === TRUE) {
+    echo "데이터베이스에 데이터 삽입 성공";
+} else {
+    echo "데이터베이스에 데이터 삽입 실패: " . $conn->error;
 }
 
+// 데이터베이스 연결 종료
 $conn->close();
 ?>
