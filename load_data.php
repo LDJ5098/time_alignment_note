a3f5f2f2c53293725801ddf009cdd18d93fc57a7
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2023-11-25 21:18:03 +0900

    Update load_data.php

--- load_data.php
+++ load_data.php
@@ -1,34 +1,32 @@
 <?php
-// 데이터베이스 연결 설정
-$servername = "localhost"; // 또는 '119.200.201.60'
+$servername = "localhost";
 $username = "timememo_user";
 $password = "5097";
 $dbname = "timememoDB";
 
-// POST 요청으로 전송된 데이터 가져오기
-$ID = $_POST['ID'];
-
-// 데이터베이스 연결
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// 연결 확인
 if ($conn->connect_error) {
     die("데이터베이스 연결 실패: " . $conn->connect_error);
 }
 
-// 입력받은 ID와 일치하는 레코드의 mainlist 값을 가져오기
+$ID = $conn->real_escape_string($_POST['ID']);
+
 $sql = "SELECT mainlist FROM user_timememo WHERE ID = '$ID'";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // 결과에서 데이터 추출
-    $row = $result->fetch_assoc();
-    $mainlist = json_decode($row['mainlist']);
-    echo json_encode($mainlist);
+if ($result) {
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $mainlist = json_decode($row['mainlist']);
+        echo json_encode($mainlist);
+    } else {
+        echo "해당 ID에 대한 데이터가 없습니다.";
+    }
 } else {
-    echo "해당 ID에 대한 데이터가 없습니다.";
+    echo "쿼리 실행 에러: " . $conn->error;
+    error_log("SQL 에러: " . $conn->error);
 }
 
-// 데이터베이스 연결 종료
 $conn->close();
 ?>
